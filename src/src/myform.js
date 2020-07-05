db866ebd23612b4eea78fb1e361f8ac1bb4ca473
Shree <shree3105@hotmail.com>
2020-07-05 15:05:20 +0100

    first commit

---
+++ src/src/myform.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+      <form
+        onSubmit={this.submitForm}
+        action="https://formspree.io/xvowkblz"
+        method="POST"
+      >
+
+
+
+
+        <input name="name" type="text" className="form-control" placeholder="name"  />
+
+
+
+        <input type="email" name="email" className="form-control" aria-describedby="emailHelp" placeholder="email"/>
+
+
+        <input name="message" type="text" className="form-control"   placeholder="message"/>
+
+
+
+        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Send my message</button>}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+      </form>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
