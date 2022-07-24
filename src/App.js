b14d09ff60bcfc9dbdfe8f17c5a536c8800d501d
Shree <shree3105@hotmail.com>
2022-07-24 13:45:37 +0100

    added photo

--- src/App.js
+++ src/App.js
@@ -10,35 +10,40 @@ function App() {
       <div class="container-fluid">
 
         <div class="nav">
-          <div class="col">
+          <div class="col pt-5">
             <h1>The Cardiology Clinic</h1>
           </div>
         </div>
 
-        <div class="row align-items-center content">
+        <div class="row align-items-center content ">
           <div class="col-1">
+
           </div>
+
           <div class="col ">
+
+            <div class="container-md p-5">
+              <img src="Photo.jpg" alt="Sujata" class="img-fluid"/>
+            </div>
 
             <p>Cardiology clinic covering all aspects of Cardiology in Poole, Dorset</p>
             <p>Poole Hospital | Nuffield Bournemouth</p>
             <p>Dr Sujata Khambekar MBBS, MD(Medicine), DNB, FRCP(UK)</p>
             <p>Partnered with all major insurance providers</p>
 
-
             <div class="forms">
-              <h5>Contact us</h5>
-              <p><strong>Email</strong> appointments@thecardiology.clinic</p>
-              <p><strong>Telephone</strong> 0300 0198255</p>
+
               <h6>Self refer</h6>
               <MyForm/>
             </div>
+
+
           </div>
 
           <div class="col-1">
           </div>
 
-          <div class="d-none d-sm-block col">
+          <div class="d-none d-sm-block col pt-5">
               <h2>About Me</h2>
               <p class="About">I am a practicing consultant cardiologist at Poole Hospital NHS Foundation Trust. specialising in Cardiac Imaging (Transthoracic Echocardiography, Transoesophageal Echocardiography, Cardiac MRI and Stress Echocardiography), Heart Failure, Atrial Fibrillation and has an interest in management of adult congenital heart disease. I am the pan-Dorset lead for Adult Congenital Heart Disease.</p>
               <p> Wide range of tests and treatments provided including: </p>
@@ -53,6 +58,10 @@ function App() {
               <p> Electrocardiogram (ECG) </p>
               <p> Event monitor </p>
               <p> MRI scan </p>
+
+              <h5>Contact us</h5>
+              <p><strong>Email</strong> appointments@thecardiology.clinic</p>
+              <p><strong>Telephone</strong> 0300 0198255</p>
           </div>
 
           <div class="col-1">
@@ -60,13 +69,15 @@ function App() {
 
         </div>
 
+
+
         <div class="footer ">
-          <div class="container">
+          <div class="container pt-5">
 
             <a href="https://finder.bupa.co.uk/Consultant/view/226316/dr_sujata_khambekar" ><img src="bupalogo3.png" alt="Bupa"/></a>
             <a href="https://www.nuffieldhealth.com/consultants/dr-sujata-khambekar" ><img src="nuffieldlogo3.png"  alt="nuffield"/></a>
 
-            <p>The entirety of this site is protected by copyright © 2019–2020 The Cardiology Clinic.</p>
+            <p>The entirety of this site is protected by copyright © 2019–2022 The Cardiology Clinic.</p>
           </div>
         </div>
       </div>
